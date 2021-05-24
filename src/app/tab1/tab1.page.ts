487d369993201174527a211162161c181e5737a6
Eduardo Silva <eduardo.silva25@fatec.sp.gov.br>
2021-05-24 11:50:41 -0300

    Adicionei o array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,35 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo: string = 'Vídeos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat',
+      lancamento: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+      nome: 'Pets Monstruosos',
+      lancamento: '02/04/2021',
+      duracao: '6m',
+      classificacao: 77,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/dkokENeY5Ka30BFgWAqk14mbnGs.jpg',
+      generos: ['Animação', 'Comédia', 'Aventura']
+    },
+    {
+      nome: 'The Good Doctor: O Bom Doutor',
+      lancamento: '01/01/2021',
+      duracao: '43m',
+      classificacao: 86,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jtLB7xJKcbekmOYkb5NZditBsgk.jpg',
+      generos: ['Drama']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
